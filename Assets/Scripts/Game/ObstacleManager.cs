5fdb5a89e2ca6104b361517c91da789a34c99a0c
Sonmv <maivietson.nd@gmail.com>
2021-01-24 21:48:41 +0700

    *[son.mv][Unity2D]: Generate obstacle

---
+++ Assets/Scripts/Game/ObstacleManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    [SerializeField] GameObject _obstaclePrefab;
+    [SerializeField] float xLimit = 3.0f;
+
+
+    void Update()
+    {
+    }
+}
